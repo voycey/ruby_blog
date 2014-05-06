de9ffb0ee8ccbcedc0a3c252780bebf67fd97ff4
dan <dan@702010forum.com>
2014-05-06 23:28:17 +1000

    Up to adding comments! http://tutorials.jumpstartlab.com/projects/blogger.html#i2:-adding-comments

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,6 @@
 class PostsController < ApplicationController
+  include PostsHelper
+
   def index
     @posts = Post.all
   end
@@ -7,5 +9,32 @@ class PostsController < ApplicationController
     @post = Post.find(params[:id])
   end
 
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+    @post.save
+    flash.notice = "Post '#{@post.title}' Created!"
+    redirect_to post_path(@post)
+  end
+
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+  def update
+    @post = Post.find(params[:id])
+    @post.update(post_params)
+    flash.notice = "Post '#{@post.title}' Updated!"
+    redirect_to post_path(@post)
+  end
+
+  def destroy
+    @post = Post.destroy(params[:id])
+    flash.notice = "Post '#{@post.title}' Deleted!"
+    redirect_to posts_path(@post)
+  end
 
 end
