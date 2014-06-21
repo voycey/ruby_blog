57ee232a93c158243849e1fce9f2b02877084ca3
Daniel Voyce <voycey@gmail.com>
2014-06-21 13:44:53 +1000

    Merge test

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
-  # This is a test comment
+  # This is a test comment - Dan Change 2
   protect_from_forgery with: :exception
 end
